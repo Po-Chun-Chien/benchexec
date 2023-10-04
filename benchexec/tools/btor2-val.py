0d6ced23ae677af30e62beee6e2b69bcead42988
Po-Chun Chien <po-chun.chien@sosy.ifi.lmu.de>
2023-10-05 01:03:14 +0200

    Simplify the way to determine result from console output

--- benchexec/tools/btor2-val.py
+++ benchexec/tools/btor2-val.py
@@ -8,7 +8,6 @@
 import benchexec.tools.template
 import benchexec.result as result
 
-import re
 
 class Tool(benchexec.tools.template.BaseTool2):
     """
@@ -20,41 +19,28 @@ class Tool(benchexec.tools.template.BaseTool2):
         return tool_locator.find_executable("main.py")
 
     def name(self):
-        return "btor2c-val"
+        return "btor2-val"
 
     def cmdline(self, executable, options, task, rlimits):
         return [executable] + options + ["--program"] + [task.single_input_file]
 
     def determine_result(self, run):
-        status = result.RESULT_UNKNOWN
+        if run.was_timeout:
+            return result.RESULT_TIMEOUT
+        pattern = "Validation result:"
         for line in run.output:
-            if "Validation result:" in line:
+            if pattern in line:
+                line = line.split(pattern)
+                if len(line) != 2:
+                    continue
+                status = line[1].strip()
+                if status.startswith(result.RESULT_TRUE_PROP):
+                    return result.RESULT_CLASS_TRUE
+                if (
+                    status.startswith(result.RESULT_FALSE_PROP)
+                    or status.startswith(result.RESULT_UNKNOWN)
+                    or status.startswith(result.RESULT_ERROR)
+                ):
+                    return status
                 # correctness cases
-                if "unknown(invariant parse failed)" in line:
-                    status = result.RESULT_UNKNOWN + "(invariant parse failed)"
-                elif "unknown(no invariant)" in line:
-                    status = result.RESULT_UNKNOWN + "(no invariant)"
-                elif "true(valid invariant)" in line:
-                    status = result.RESULT_TRUE_PROP
-                elif "true(safe invariant)" in line:
-                    status = result.RESULT_TRUE_PROP
-                elif "true(inductive invariant)" in line:
-                    status = result.RESULT_TRUE_PROP
-                elif "unknown(invalid invariant)" in line:
-                    status = result.RESULT_UNKNOWN + "(invalid invariant)"
-                elif "unknown(unsafe invariant)" in line:
-                    status = result.RESULT_UNKNOWN + "(unsafe invariant)"
-                elif "unknown(not inductive invariant)" in line:
-                    status = result.RESULT_UNKNOWN + "(not inductive invariant)"
-                # violation cases
-                elif "false" in line:
-                    status = result.RESULT_FALSE_PROP
-                elif "unknown(violation not reached)" in line:
-                    status = result.RESULT_UNKNOWN + "(violation not reached)"
-                # common cases
-                elif "error" in line:
-                    pattern = r"error(\([^)]+\))"
-                    cause = str(re.search(pattern, line).group(1))
-                    status = result.RESULT_ERROR + cause
-                break
-        return status
+        return result.RESULT_ERROR
